d2d017edca34eb9fce939488cb520c98a939ee7b
Aaron <mcdulltii@gmail.com>
2021-04-05 18:16:49 +0800

    Update install-node.sh

--- etc/install-node.sh
+++ etc/install-node.sh
@@ -34,7 +34,7 @@ ${RESET}"
         done
         if [[ "$user_prompt" == [Yy]* ]]; then
             echo -e "\n${GREEN}Installing node.js into ~/.local/ ...${RESET}";
-            dotfiles install node && exit 0;
+            ~/.local/bin/dotfiles install node && exit 0;
         fi
     fi
 
